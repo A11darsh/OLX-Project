18d0ede1553ef5c427db961f1296a0d8300e725e
adarsh <adarshmishra5432@gmail.com>
2022-05-10 15:54:15 +0530

    complete

--- OLX-Advertize/src/main/java/com/zensar/advertize/config/SecurityConfig.java
+++ OLX-Advertize/src/main/java/com/zensar/advertize/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.zensar.advertize.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,6 +19,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	
 	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+	}
+
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
